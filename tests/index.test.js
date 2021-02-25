395e04596fda0859af4b24bbe1f4ff1da86084b2
Daniel <daniel@lngzl.nl>
2021-02-25 13:27:33 +0100

    ✅ Testing altering the chain

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,35 @@
 const Chainson = require('../lib/index')
 
-test('Hello world test', async () => {
-  const chain = new Chainson()
-  chain.add('hello', 'world')
-
-  const res = chain.get('hello')
-  expect(res).toEqual('world')
-})
-
-test('other words test', async () => {
-  const chain = new Chainson()
-  chain.add('peter', 'limonade')
-
-  const res = chain.get('peter')
-  expect(res).toEqual('limonade')
+describe('Altering chain', () => {
+  const chain = new Chainson(chainLocation = './testchain')
+  
+  test('Add to chain', () => {
+    chain.add('hello', 'world')
+  
+    const res = chain.get('hello')
+    expect(res).toEqual('world')
+  })
+  
+  test('Not in chain', () => {
+    try {
+      chain.get('peter')
+    } catch(e) {
+      expect(e.message).toMatch('Link `peter` not found in Chain')
+    }
+  })  
+  
+  test('Updating in chain', () => {
+    chain.update('hello', 'limonade')
+    const res = chain.get('hello')
+    expect(res).toEqual('limonade')
+  })
+  
+  test('Removing from chain', () => {
+    chain.remove('hello')
+    try {
+      chain.get('hello')
+    } catch(e) {
+      expect(e.message).toMatch('Link `hello` not found in Chain')
+    }
+  })
 })
