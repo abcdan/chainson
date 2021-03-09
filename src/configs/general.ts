b36d2fb721a1af96eead04b23a949d07b1804426
Daniel <daniel@lngzl.nl>
2021-03-09 16:18:47 +0100

    🔖 0.0.13-a (critical bug)

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,7 +1,7 @@
 const general = {
   // Do not change this, we'll change it when we release a new version.
   // TODO: Making this automatic when you run `npm prepare-for-release`
-  version: '0.0.12-a',
+  version: '0.0.13-a',
 };
 
 export { general };
