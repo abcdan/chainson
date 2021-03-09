914d1acba7d1261bf3980af6dc297ddb0926e457
Daniel <daniel@lngzl.nl>
2021-03-09 17:27:29 +0100

    🔖 Updating to version 0.0.14-a

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,7 +1,7 @@
 const general = {
   // Do not change this, we'll change it when we release a new version.
   // TODO: Making this automatic when you run `npm prepare-for-release`
-  version: '0.0.13-a',
+  version: '0.0.14-a',
 };
 
 export { general };
